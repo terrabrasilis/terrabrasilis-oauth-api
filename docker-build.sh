10ec1bce6c19bfdf53f7e25ea53a3991c04677d6
Claudio Henrique Bogossian <claudio.bogossian@gmail.com>
2020-03-17 10:15:34 -0300

    docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,18 +2,6 @@
 
 VERSION=$(cat package.json | grep -oP '(?<="version": ")[^"]*')
 
-# Stopping all containers
-#docker container stop terrabrasilis_amazon_alert_daily
-#docker container stop terrabrasilis_amazon_alert_aggregated
+docker build -t terrabrasilis/terrabrasilis-oauth-api:v$VERSION -f Dockerfile .
 
-# build all images
-docker build -t terrabrasilis/terrabrasilis-oauth-api:v$VERSION --build-arg -f Dockerfile .
-
-# send to dockerhub
-## docker login
 docker push terrabrasilis/terrabrasilis-oauth-api:v$VERSION
-
-# If you want run containers, uncomment this lines
-#docker run -d --rm -p 83:80 --name terrabrasilis_amazon_alert_daily terrabrasilis/amazon-alert-daily:v$VERSION
-#docker run -d --rm -p 84:80 --name terrabrasilis_amazon_alert_aggregated terrabrasilis/amazon-alert-aggregated:v$VERSION
-
